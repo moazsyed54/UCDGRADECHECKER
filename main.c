31950d3796604e60e360da9dd4394cfc9d827510
Mohammad Syed <39340965+moazsyed54@users.noreply.github.com>
2018-07-06 23:36:46 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main (){
+
+float ca,fin;
+printf("please enter the continous assemment grade and the worth of the final exam");
+float arr[]={76.77,73.33,70.00};
+scanf("%f %f",&ca,&fin);
+
+
+
+
+
+
+
+
+}
