10e85453b3df8d60d69e39a2bdddbe841e4c0c21
Mohammad Syed <39340965+moazsyed54@users.noreply.github.com>
2018-07-06 23:32:16 +0100

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -23,9 +23,9 @@ public class MainActivity extends AppCompatActivity {
         char grade[]={'A','A','A'};
         char sign[]={'+',' ','-'};
 
-        double a,b,c,d;
+        double a,b,c,d; //intialising some variables :)
 
-        double i = 76.67 - Double.parseDouble(password.getText().toString());
+        double i = 76.67 - Double.parseDouble(password.getText().toString()); // the Double.parseDouble changes from a string to double 
         double j = 100*(i / Double.parseDouble(username.getText().toString()));
         double z = Math.round(j * 100.0) / 100.0;
         String total2 = String.valueOf(z);
